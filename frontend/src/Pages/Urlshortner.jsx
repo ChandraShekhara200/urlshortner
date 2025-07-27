e5d5a848a95c621b10e2d6bc11a7f023f392ad46
chandu <23311a12k4@it.sreenidhi.edu.in>
2025-07-27 17:49:34 +0530

    finalpush2

--- frontend/src/Pages/Urlshortner.jsx
+++ frontend/src/Pages/Urlshortner.jsx
@@ -1,6 +1,8 @@
+
 import { Button, Container, TextInput } from '@mantine/core'
 import React, { useState } from 'react'
 import Service from '../utils/http'
+import Response from '../Components/Response';
 
 
 const service = new Service();
@@ -13,6 +15,8 @@ export default function UrlShortener() {
        console.log(input?.originalUrl);
        try {
            const data = await service.post("s", input);
+           
+           setResponse(data);
            console.log(data);
        } catch (error) {
            console.error("Error generating short URL:", error);
@@ -28,45 +32,55 @@ export default function UrlShortener() {
    });
 
 
+   const [response, setResponse] = useState(null);
+
+
    return (
        <Container size={"xs"}>
-           URL Shortener
-           <TextInput
-               size="lg"
-               label="Original Url "
-               withAsterisk
-               placeholder="Input placeholder"
-               onChange={(e) => { setInput({ ...input, originalUrl: e.target.value }) }}
-           />
-           <TextInput
-               size="lg"
-               label="Custom Url "
-               placeholder="Input placeholder"
-               onChange={(e) => { setInput({ ...input, customUrl: e.target.value }) }}
-           />
-           <TextInput
-               size="lg"
-               label="Title"
-               placeholder="Input placeholder"
-               onChange={(e) => { setInput({ ...input, title: e.target.value }) }}
-           />
-           <TextInput
-               size="lg"
-               type='date'
-               label="expiresAt"
-               placeholder="Input placeholder"
-               onChange={(e) => { setInput({ ...input, expiresAt: e.target.value }) }}
-           />
-           <Button
-               onClick={generateShortUrl}
-               variant="outline"
-               color="cyan"
-               size="lg"
-               radius="lg">
+
+
+           {!response?
+           <>
+               URL Shortener
+               <TextInput
+                   size="lg"
+                   label="Original Url "
+                   withAsterisk
+                   placeholder="Input placeholder"
+                   onChange={(e) => { setInput({ ...input, originalUrl: e.target.value }) }}
+               />
+               <TextInput
+                   size="lg"
+                   label="Custom Url "
+                   placeholder="Input placeholder"
+                   onChange={(e) => { setInput({ ...input, customUrl: e.target.value }) }}
+               />
+               <TextInput
+                   size="lg"
+                   label="Title"
+                   placeholder="Input placeholder"
+                   onChange={(e) => { setInput({ ...input, title: e.target.value }) }}
+               />
+               <TextInput
+                   size="lg"
+                   type='date'
+                   label="expiresAt"
+                   placeholder="Input placeholder"
+                   onChange={(e) => { setInput({ ...input, expiresAt: e.target.value }) }}
+               />
+               <Button
+                   onClick={generateShortUrl}
+                   variant="outline"
+                   color="cyan"
+                   size="lg"
+                   radius="lg">
                    Button
-           </Button>;
-
-
+               </Button>;
+           </>
+           :
+           <Response response={response} setResponse={setResponse}/>
+           }
        </Container>
    )
 }
+
